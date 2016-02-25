4d58f4dff1397330f5db6fc190e9d9da541c8e1d
johntr <jtredlich@gmail.com>
2016-02-25 15:00:06 -0500

    Added route for resource type. Updated paths follow a standard logic.

--- index.php
+++ index.php
@@ -4,19 +4,20 @@ require 'vendor/autoload.php';
 require 'config.php';
 
 use RamSources\ResourceLoaders\ResourceLoader;
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 $r = new ResourceLoader($dbconfig);
-
 $app = new \Slim\App();
 
-$app->get('/', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) use ($r) {
+$app->get('/api/v1/resources', function (Request $request, Response $response, $args) use ($r) {
   $response->withStatus(200);
   $response->getBody()->write(json_encode($r->getResources()));
   $newResponse = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
   return $newResponse;
 });
 
-$app->get('/resource/{id}', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) use ($r) {
+$app->get('/api/v1/resource/{id}', function (Request $request, Response $response, $args) use ($r) {
   $id = $args['id'];
   $response->withStatus(200);
   $response->getBody()->write(json_encode($r->getResources($id)));
@@ -24,7 +25,15 @@ $app->get('/resource/{id}', function (\Slim\Http\Request $request, \Slim\Http\Re
   return $newResponse;
 });
 
-$app->get('/building/{bid}', function(\Slim\Http\Request $request, \Slim\Http\Response $response, $args) use ($r) {
+$app->get('/api/v1/resource/type/{type}', function(Request $request, Response $response, $args) use ($r) {
+  $t = $args['type'];
+  $response->withStatus(200);
+  $response->getBody()->write(json_encode($r->getResourceByType($t)));
+  $newResponse = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
+  return $newResponse;
+});
+
+$app->get('/api/v1/building/{bid}', function(Request $request, Response $response, $args) use ($r) {
   $bid = $args['bid'];
   $response->withStatus(200);
   $response->getBody()->write(json_encode($r->getResourceByBuilding($bid)));
