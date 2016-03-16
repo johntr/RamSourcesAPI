1d36e3bd84a25ad6b3dad8164ccb3595e03158ca
johntr <jtredlich@gmail.com>
2016-03-15 23:56:58 -0400

    Added and tested function to add comment. Added functions to get and delete comments but untested. Tired.

--- src/ResourceLoaders/CommentLoader.php
+++ src/ResourceLoaders/CommentLoader.php
@@ -1,10 +1,65 @@
 <?php
 
 namespace RamSources\ResourceLoaders;
+use RamSources\Database\Database;
 
 class CommentLoader {
 
+  private $conn;
+
+  function __construct($dbconfig) {
+
+    $this->conn = new Database($dbconfig);
+  }
+
   function getCommentsByResource($rid) {
+    $sql = "SELECT * From `Comments` WHERE resource_id = :rid";
+
+    $this->conn->query($sql);
+    $this->conn->bind(':rid', $rid);
+    $this->conn->execute();
+    return $this->conn->results();
+  }
+
+  function addComment($data) {
+
+    $sql = "INSERT INTO `Comments` (parent_comment, comment, date_stamp , user_id, resource_id) VALUES (:parent_comment, :comment, :dates, :user_id, :resource_id)";
+    $this->conn->query($sql);
+    try {
+      foreach ($data as $k => $v) {
+        $this->conn->bind(':' . $k, $v);
+      }
+      $date = date('Y-m-d H:i:s', strtotime('now'));
+      $this->conn->bind(':dates', $date);
+      $this->conn->execute();
+
+      $message = array('Result' => $this->conn->lastInsertId());
+      return $message;
+    } catch (\PDOException $e) {
+      echo $this->conn->debugDumpParams();
+      $message = array('Result' => $e->getMessage());
+      return $message;
+    }
+
+  }
+
+  function removeComment($cid) {
+    $sql = "DELETE FROM `Comments` WHERE comment_id = :cid";
+
+    $this->conn->query($sql);
+    try {
+      $this->conn->beginTransaction();
+      $this->conn->bind(':cid', $cid);
+      $this->conn->execute();
+      $this->conn->endTransaction();
+      $message = array('Result' => 'Success');
+      return $message;
+    }
+    catch(\PDOException $e) {
+      $this->conn->cancelTransaction();
+      $message = array('Result' => $e->getMessage());
+      return $message;
+    }
 
   }
 }
