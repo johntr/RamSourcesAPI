b0a469b1a903a40116bc77f97f62709ac4226b40
johntr <jtredlich@gmail.com>
2016-03-22 23:10:39 -0400

    Added route to create new resources

--- app/routes/resource_routes.php
+++ app/routes/resource_routes.php
@@ -14,8 +14,6 @@ $app->group('/resource', function() use ($app, $dbconfig) {
     $newResponse = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
     return $newResponse;
   });
-
-
   $app->get('/id/{id}', function (Request $request, Response $response, $args) use ($r) {
     $id = $args['id'];
     $response->withStatus(200);
@@ -44,6 +42,17 @@ $app->group('/resource', function() use ($app, $dbconfig) {
     $newResponse = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
     return $newResponse;
   });
+  
+  $app->post('/new', function (Request $request, Response $response, $args) use ($r) {
+    $parsedData = $request->getParsedBody();
+    $updateResponse = $r->addResource($parsedData);
+
+    $response->withStatus(200);
+    $response->getBody()->write(json_encode($updateResponse));
+    $newResponse = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
+    return $newResponse;
+  });
+  
   $app->put('/update/id/{id}', function (Request $request, Response $response, $args) use ($r) {
 
     $parsedData = $request->getParsedBody();
