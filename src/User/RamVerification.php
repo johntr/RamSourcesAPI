2f92728fdf737f301b033e0f0be730ae0e541376
johntr <jtredlich@gmail.com>
2016-04-02 16:07:33 -0400

    Created class to send verification emails and update account as email verified

---
+++ src/User/RamVerification.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace RamSources\User;
+use RamSources\Database\Database;
+use PHPMailer;
+
+class RamVerification {
+
+
+  private $db;
+  private $mail;
+  private $hash;
+  private $userInfo = array();
+
+  function __construct($dbconfig) {
+
+    $this->db = new Database($dbconfig);
+    $this->mail = new PHPMailer();
+
+    $this->mail->Host = 'localhost';
+    $this->mail->Port = 587;
+    $this->mail->setFrom('no-reply@ramsources.com', 'Ramsources Email Validation');
+    $this->mail->isHTML(true);
+
+  }
+  function sendVerify($userInfo) {
+    $this->userInfo = $userInfo;
+    $this->hash = bin2hex(openssl_random_pseudo_bytes(4));
+    $date = strtotime('now');
+
+    $sql = "INSERT INTO `Verification` (verify_time, verify_hash, user_id, status) VALUES (:vdate, :vhash, :id ,:status)";
+    try {
+      $this->db->query($sql);
+      $this->db->bind(':vdate', $date);
+      $this->db->bind(':vhash', $this->hash);
+      $this->db->bind(':id', $this->userInfo['id']);
+      $this->db->bind(':status', 0);
+      //$this->db->execute();
+
+      $this->sendVerifyMail();
+    }
+    catch(\PDOException $e) {
+      echo $e->getMessage();
+    }
+    catch(\Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  private function sendVerifyMail() {
+    $this->mail->addAddress($this->userInfo['email'], $this->userInfo['name']);
+    $this->mail->addBCC('jtredlich@gmail.com', 'John Redlich');
+    $this->mail->Subject = 'Ramsources Email Verification';
+    $HTMLbody = $this->_createHTMLBody();
+    $TXTbody = strip_tags($HTMLbody);
+    $this->mail->Body = $HTMLbody;
+    $this->mail->AltBody = $TXTbody;
+
+    if (!$this->mail->send()) {
+      throw new \Exception($this->mail->ErrorInfo);
+    }
+    else {
+      echo "Sent";
+    }
+  }
+
+  function getIdFromHash($hash) {
+    $this->hash = $hash;
+    $updateHash = "UPDATE `Verification` SET status=1 WHERE verify_hash=:hash";
+    $getID = "SELECT user_id, status FROM `Verification` WHERE verify_hash = :hash";
+
+    try {
+      $this->db->beginTransaction();
+      $this->db->query($getID);
+      $this->db->bind(':hash', $this->hash);
+      $this->db->execute();
+      $userID = $this->db->single();
+
+      $this->db->query($updateHash);
+      $this->db->bind(':hash', $this->hash);
+      $this->db->execute();
+      $this->db->endTransaction();
+      if ($userID['status'] == 0) {
+        return $userID['user_id'];
+      }
+      else {
+        return array('Result' => 'Fail', "Message" => "Hash already used");
+      }
+    }
+    catch(\PDOException $e) {
+      $this->db->cancelTransaction();
+      echo $e->getMessage();
+    }
+  }
+
+  private function _createHTMLBody() {
+    $body = "<p>Hello {$this->userInfo['name']},<br/>Thank you for creating a RamSources account. RamSources is a service for SUNY Farmingdale students, we will need you to verify your account by either clicking the link below or copying it into a browser.<br/><a href='http://www.ramsources.com/app/userverify?id=$this->hash'>http://www.ramsources.com/app/userverify?id=$this->hash</a></p>";
+    return $body;
+  }
+
+}
