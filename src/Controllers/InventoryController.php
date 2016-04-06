df1c9782536ae41e036dfb153a70f36d807995c2
johntr <jtredlich@gmail.com>
2016-04-05 23:36:40 -0400

    Added Inventory Controller class. We can now manage our Ramventory

---
+++ src/Controllers/InventoryController.php
@@ -0,0 +1,58 @@
+<?php
+namespace RamSources\Controllers;
+
+use RamSources\Utils\Database;
+use RamSources\Utils\Logging;
+
+class InventoryController {
+
+  private $db;
+  private $log;
+
+
+  function __construct($dbconfig) {
+    $this->db = new Database($dbconfig);
+    $this->log = new Logging();
+  }
+
+  function getInventoryById($id) {
+
+    $sql = "SELECT inv_type, inv_name FROM `Ramventory` WHERE resource_id = :id ";
+
+    try {
+      $this->db->query($sql);
+      $this->db->bind(':id', $id);
+      $this->db->execute();
+
+      $results = $this->db->results();
+
+      return array ('result' => 'Success', 'message' => $results);
+    }
+    catch(\PDOException $e) {
+      $this->log->logError($e);
+      return array('result' => 'Failure', 'message' => $e->getMessage());
+    }
+  }
+
+  function addInventory($rid,$inv) {
+    
+    $sql = "INSERT INTO (inv_type, inv_name, resource_id) VALUES (:type, :name, :rid)";
+    
+    try {
+      $this->db->beginTransaction();
+      foreach($inv as $k =>$v) {
+        $this->db->query($sql);
+        $this->db->bind('rid', $rid);
+        $this->db->bind(':type', $k);
+        $this->db->bind(':name', $v);
+        $this->db->execute();
+      }
+      $this->db->endTransaction();
+    }
+    catch(\PDOException $e) {
+      $this->db->cancelTransaction();
+      $this->log->logError($e);
+      return array('result' => 'Failure', 'message' => $e->getMessage());
+    }
+  }
+}
