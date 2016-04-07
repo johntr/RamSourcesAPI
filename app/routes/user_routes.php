28298c1e672f0dee1373762350fdef02fb8ba1c1
johntr <jtredlich@gmail.com>
2016-04-06 21:45:30 -0400

    Changed route for user creation to a post request

--- app/routes/user_routes.php
+++ app/routes/user_routes.php
@@ -9,10 +9,33 @@ $app->group('/user', function() use ($app, $dbconfig) {
   $u = new RamUser($dbconfig);
   $v = new RamVerification($dbconfig);
 
-  $app->get('/new/{user}/{pass}/{name}', function(Request $request, Response $response, $args) use ($u) {
-    $user = $args['user'];
-    $pass = $args['pass'];
-    $rname = explode("-", $args['name']);
+
+  $app->get('/login', function(Request $request, Response $response, $args) use ($u) {
+    $authHeader = $request->getHeader('Authorization');
+    $userInfo = $u->decodeHeader($authHeader[0]);
+    $u->getUser($userInfo[0]);
+
+    try {
+      $u->verifyPass($userInfo[0], $userInfo[1]);
+      $response->withStatus(200);
+      $response->getBody()->write(json_encode($u->createUserToken()));
+      $newresponce = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
+      return $newresponce;
+    }
+    catch (Exception $e) {
+      $status = array('result' => 'Fail', 'message' => $e->getMessage());
+      $response->withStatus(500);
+      $response->getBody()->write(json_encode($status));
+      $newresponce = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
+      return $newresponce;
+    }
+  });
+
+  $app->post('/new', function(Request $request, Response $response, $args) use ($u) {
+    $userDta = $request->getParsedBody();
+    $user = $userDta['user'];
+    $pass = $userDta['pass'];
+    $rname = explode("-", $userDta['name']);
     $name = $rname[0] . " " . $rname[1];
     $userDomain = explode('@', $user);
 
@@ -33,27 +56,6 @@ $app->group('/user', function() use ($app, $dbconfig) {
     else {
       $response->withStatus(401);
       $status = ['result' => 'Fail', 'message' => 'RamSources for SUNY Farmingdale students only, Farmingdale.edu email required.'];
-      $response->getBody()->write(json_encode($status));
-      $newresponce = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
-      return $newresponce;
-    }
-  });
-
-  $app->get('/login', function(Request $request, Response $response, $args) use ($u) {
-    $authHeader = $request->getHeader('Authorization');
-    $userInfo = $u->decodeHeader($authHeader[0]);
-    $u->getUser($userInfo[0]);
-
-    try {
-      $u->verifyPass($userInfo[0], $userInfo[1]);
-      $response->withStatus(200);
-      $response->getBody()->write(json_encode($u->createUserToken()));
-      $newresponce = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
-      return $newresponce;
-    }
-    catch (Exception $e) {
-      $status = array('result' => 'Fail', 'message' => $e->getMessage());
-      $response->withStatus(500);
       $response->getBody()->write(json_encode($status));
       $newresponce = $response->withHeader('Content-type', 'application/json; charset=UTF-8');
       return $newresponce;
