97ffdcc10071d07c849633bd58b929f19d5ff90d
johntr <jtredlich@gmail.com>
2016-04-14 23:04:50 -0400

    Added comments to utility classes.

--- src/Utils/Database.php
+++ src/Utils/Database.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Database utility class. Basically a wrapper around PDO.
+ *
+ * Created by John Redlich for the RamSources project.
+ * Spring 2016
+ *
+ */
 namespace RamSources\Utils;
 
 use \PDO;
@@ -15,6 +21,10 @@ class Database {
   private $stmt;
 
 
+  /**
+   * Database constructor. Sets up config for database connection. Bam DB Class baby.
+   * @param $dbconfig
+   */
   function __construct($dbconfig) {
 
     $this->dbname = $dbconfig['db'];
@@ -22,16 +32,27 @@ class Database {
     $this->user = $dbconfig['un'];
     $this->pass = $dbconfig['pw'];
 
-
+    //db config based on config variables. @TODO user container config. maybe.
     $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+    //set pdo to throw exceptions.
     $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
   }
 
 
+  /**
+   * Prepare sql query. Oh shit, here we go.
+   * @param $q
+   */
   public function query($q) {
     $this->stmt = $this->conn->prepare($q);
   }
 
+  /**
+   * Type sql variables and bind them to the query. SQL Injections are the bad.
+   * @param $param
+   * @param $value
+   * @param null $type
+   */
   public function bind($param, $value, $type = null) {
     if(is_null($type)) {
       switch(true) {
@@ -44,6 +65,7 @@ class Database {
         case is_null($value):
           $type = PDO::PARAM_NULL;
           break;
+        //do string last since php thinks everything are strings.
         default:
           $type = PDO::PARAM_STR;
       }
@@ -51,40 +73,76 @@ class Database {
     $this->stmt->bindValue($param, $value, $type);
   }
 
+  /**
+   * Send query off to DB. Lets get this party started.
+   * @return mixed
+   */
   public function execute() {
     return $this->stmt->execute();
   }
 
+  /**
+   * Get results as Associated array. Because PHP arrays are the most awesome.
+   * @return mixed
+   */
   public function results() {
     $this->execute();
     return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
+  /**
+   * Get single record back from db. Good for verifying single result for get ids and such.
+   * @return mixed
+   */
   public function single() {
     $this->execute();
     return $this->stmt->fetch(PDO::FETCH_ASSOC);
   }
 
+  /**
+   * Get result row count.
+   * @return mixed
+   */
   public function rowCount() {
     return $this->stmt->rowCount();
   }
 
+  /**
+   * Get last inserted id.
+   * @return string
+   */
   public function lastInsertId() {
     return $this->conn->lastInsertId();
   }
 
+  /**
+   * Start db transactionf or multi queries. Fingers crossed.
+   * @return bool
+   */
   public function beginTransaction() {
     return $this->conn->beginTransaction();
   }
 
+  /**
+   * Commit the transaction. Hold onto your butts.
+   * @return bool
+   */
   public function endTransaction() {
     return $this->conn->commit();
   }
 
+  /**
+   * Rolls back bad commit. Not uh, uh you didn't say the magic word.
+   * @return bool
+   */
   public function cancelTransaction() {
     return $this->conn->rollBack();
   }
 
+  /**
+   * Dump the bind values. Lets see what this query is expecting.
+   * @return mixed
+   */
   public function debugDumpParams() {
     return $this->stmt->debugDumpParams();
   }
--- src/Utils/Logging.php
+++ src/Utils/Logging.php
@@ -1,7 +1,14 @@
 <?php
-
+/**
+ * Loggind. Uses monolog to log errors and notifications to file log.
+ * There are 4 levels to log. @TODO Maybe we need 5, have a nice middle ground. Idk.
+ *
+ * Created by John Redlich for the RamSources project.
+ * Spring 2016
+ *
+ */
 namespace RamSources\Utils;
-
+//get a monolog object.
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
@@ -10,25 +17,46 @@ class Logging {
   private $log;
   private $stream;
 
+  /**
+   * Logging constructor.
+   * Start a basic filestream to pass logs entries to.
+   */
   function __construct() {
 
     $this->log = new Logger('RamSource');
+    //Start the file stream. @TODO Need to get this in config so we can use on server.
     $this->stream = new StreamHandler($_SERVER['DOCUMENT_ROOT'] . '/logs/RamLogs.log', Logger::WARNING);
     $this->log->pushHandler($this->stream);
   }
-  
+
+  /**
+   * Level 1 notification. Oh, good to know.
+   * @param $message
+   */
   function logNotification($message) {
     $this->log->addNotice($message);
   }
 
+  /**
+   * Level 2 notification. Meh, ok.
+   * @param $message
+   */
   function logWarning($message) {
     $this->log->addWarning($message);
   }
 
+  /**
+   * Level 3 notification. Shit. What is going wrong here.
+   * @param $message
+   */
   function logError($message) {
     $this->log->addError($message);
   }
 
+  /**
+   * Level 4 notification. Fuck, I never want to do development again.
+   * @param $message
+   */
   function logCritical($message) {
     $this->log->addCritical($message);
   }
